90e88fa934c7a2e8b4d792161a9fab201d7411db
unexpecteds <42111535+unexpecteds@users.noreply.github.com>
2023-09-09 15:39:03 +0800

    Create Main.java

---
+++ app/src/main/java/com/umexpected/disable/security/windows/Main.java
@@ -0,0 +1,40 @@
+package com.unexpected.hook.disable.security.windows;
+
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+
+public class Main implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        // 禁用安全窗口
+        XposedHelpers.findAndHookMethod(Window.class, "setFlags", int.class, int.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                    Integer flags = (Integer) param.args[0];
+                    flags &= ~ WindowManager.LayoutParams.FLAG_SECURE;
+                    param.args[0] = flags;
+                }
+            });
+        // 禁用安全视图
+        XposedHelpers.findAndHookMethod(SurfaceView.class, "setSecure", boolean.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                    param.args[0] = false;
+                }
+            });
+        // 禁用触摸过滤，允许被遮挡时执行点击事件
+        XposedHelpers.findAndHookMethod(View.class, "setFilterTouchesWhenObscured", boolean.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                    param.args[0] = false;
+                }
+            });
+    }
+}
